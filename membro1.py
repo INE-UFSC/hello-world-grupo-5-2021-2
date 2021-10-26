a326399240543d3c6c453ac325488e9dfcb720d5
marianadick <dickmariana1@gmail.com>
2021-10-26 11:25:47 -0300

    Apagando as informações extras

--- membro1.py
+++ membro1.py
@@ -1,4 +1,3 @@
 def hello():
 	return "Olá, meu nome é Mariana, tenho 20 anos. Meus principais hobbies envolvem atividades físicas, gosto de andar, correr, fazer trilhas e academia. A experiência que eu possuo até então foi construída basicamente em POO I, e agora no projeto de pesquisa onde estou envolvida. Com a disciplina de POO II eu planejo obter uma base sólida em programação orientada a objetos e aprimorar minhas práticas de desenvolvimento." 
-	
-	#escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+
