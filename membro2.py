dc257ecaa6664c3cd7d8ad8eea71815a8cd4b623
ctc <gabrieljacquesvilela@gmail.com>
2021-10-26 11:17:49 -0300

    Adicionadas as informações sobre o membro 2 do grupo 5.

--- membro2.py
+++ membro2.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Gabriel Jacques, tenho 20 anos e meu hobbie favorito é tocar violão (que nada tem a ver com computação kkkk); minha experiência com programação começou com POO I e o que eu espero com POO II é aprender ainda mais sobre o mundo da programação e poder pôr em prática os conteúdos aprendidos em minha vida profissional durante ou após o término da disciplina."
