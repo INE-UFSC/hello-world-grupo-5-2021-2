497dc6b6de33d3ac2d0f8981a2aa7591a5a818e4
Yuri <yuri-zimmermann@outlook.com>
2021-11-05 10:02:31 -0300

    informações do membro 4

---
+++ membro4.py
@@ -0,0 +1,4 @@
+def hello():
+	return "Olá, eu sou o membro4 do Grupo 5 - 2021.2.\nMeu nome é Yuri Zimmermann e tenho 24 anos. Meus hobbies: Jogos online, e alguns esportes(basquete, handebol, rugby...)" \
+		   "\nComecei a aprender programação no semestre passado então minha experiencia é pouca, basicamente o que vimos em poo-I"\
+		   "\nEu espero esse semestre aprender mais sobre a orientação a objetos em si e aprender a organizar melhor os meus codigos"
